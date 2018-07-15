22ab54e872b41bcc8b12b9ab61dee53031f4b054
msvsr <msvsr11297@gmail.com>
2018-07-15 11:23:18 +0530

    deploying osqa app

--- osqaapp/urls.py
+++ osqaapp/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from osqa import settings
+#from osqa import settings
 from osqaapp.form import AddQcomment
 from osqaapp.views.Answers import AnswerView, ALikeCreate, Addanswers, AnswerListView, EditAnswerView
 from osqaapp.views.Question import QuestionView, AddQuestions, QuestionListView, DeleteQuestionView, EditQuestionView, \
